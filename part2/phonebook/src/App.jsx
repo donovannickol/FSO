a269e91d64784539dd3ddf223e1359bddf9c1e83
tdn9 <tdn9@duke.edu>
2024-09-30 22:24:08 -0400

    added json server db to phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Person = ({person}) => <li>{person.name} {person.number}</li>
 
@@ -29,57 +30,53 @@ const PersonForm = ({addPerson, handleNameChange, handleNumberChange, newName, n
 )
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchQuery, setSearchQuery] = useState('')
 
-  
-const addPerson = (event) => {
-  event.preventDefault()
-  if(persons.some(person => person.name === newName)){
-    alert(`${newName} is already added to phonebook`)
-  }
-  else{
-    const personObject = {
-      name: newName,
-      number: newNumber,
-      id: persons.length + 1
+  useEffect(() => axios.get('http://localhost:3001/persons').then(response => setPersons(response.data)), [])
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    if(persons.some(person => person.name === newName)){
+      alert(`${newName} is already added to phonebook`)
     }
-    setPersons(persons.concat(personObject))
+    else{
+      const personObject = {
+        name: newName,
+        number: newNumber,
+        id: persons.length + 1
+      }
+      setPersons(persons.concat(personObject))
+    }
+    setNewName('')
+    setNewNumber('')
   }
-  setNewName('')
-  setNewNumber('')
-}
 
-const handleNameChange = (event) => {
-  setNewName(event.target.value)
-}
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
 
-const handleNumberChange = (event) => {
-  setNewNumber(event.target.value)
-}
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
 
-const handleSearch = (event) => {
-  setSearchQuery(event.target.value)
-}
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value)
+  }
 
-const queryPersons = searchQuery === '' ? persons : persons.filter(person => person.name.toUpperCase().includes(searchQuery.toUpperCase()))
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <Filter searchQuery={searchQuery} handleSearch={handleSearch}/>
-      <h2>add a new</h2>
-      <PersonForm addPerson={addPerson} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} newName={newName} newNumber={newNumber} />
-      <h2>Numbers</h2>
-      <Persons queryPersons={queryPersons}/>
-    </div>
-  )
+  const queryPersons = searchQuery === '' ? persons : persons.filter(person => person.name.toUpperCase().includes(searchQuery.toUpperCase()))
+    return (
+      <div>
+        <h2>Phonebook</h2>
+        <Filter searchQuery={searchQuery} handleSearch={handleSearch}/>
+        <h2>add a new</h2>
+        <PersonForm addPerson={addPerson} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} newName={newName} newNumber={newNumber} />
+        <h2>Numbers</h2>
+        <Persons queryPersons={queryPersons}/>
+      </div>
+    )
 }
 
 export default App
